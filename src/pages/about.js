b471a08ea1d4f767b9e00d84c0383e91de27828e
Lee Wise <theleewise@gmail.com>
2020-02-10 21:37:33 -0600

    Testing contact form

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,12 @@ const About = (location) => (
                 <h1>About Us</h1>
                 <p>Do cupidatat officia do voluptate nostrud nisi. Reprehenderit amet aute sint consectetur qui aliqua occaecat culpa duis Lorem. Reprehenderit quis sint nostrud nostrud. Dolor quis tempor eiusmod in minim laborum sint elit enim et commodo nulla cupidatat fugiat.</p>
             </div>
+            <form name="contact" method="POST" data-netlify="true">
+                <label>Name: <input type="text" name="name" placeholder="Name" /></label>
+                <label>Email: <input type="email" name="email" placeholder="Email" /></label>
+                <label>Message: <textarea name="message">Message</textarea></label>
+                <button type="submit">Send</button>
+            </form>
     </Layout>
 )
 
