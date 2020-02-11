377cacb2e335af4640220575ccf1bffd0450f473
Lee Wise <theleewise@gmail.com>
2020-02-10 21:55:02 -0600

    Fix type

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const About = (location) => (
                 <h1>Contact Us</h1>
                 <p>Do cupidatat officia do voluptate nostrud nisi. Reprehenderit amet aute sint consectetur qui aliqua occaecat culpa duis Lorem. Reprehenderit quis sint nostrud nostrud. Dolor quis tempor eiusmod in minim laborum sint elit enim et commodo nulla cupidatat fugiat.</p>
             </div>
-            <form name="contact" method="POST" data-netlify="true" action="/contact/successs">
+            <form name="contact" method="POST" data-netlify="true" action="/contact/success">
                 <label>Name: <input type="text" name="name" placeholder="Name" /></label>
                 <label>Email: <input type="email" name="email" placeholder="Email" /></label>
                 <label>Message: <textarea name="message">Message</textarea></label>
