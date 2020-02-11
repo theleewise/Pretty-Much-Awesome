c5ee1375b7a936700c3043f99a22e1cb7cf7b7f2
Lee Wise <theleewise@gmail.com>
2020-02-10 21:54:21 -0600

    Trying to get form to work

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,9 +5,15 @@ import Layout from '../components/layout';
 const About = (location) => (
     <Layout location={location}>
             <div>
-                <h1>About Us</h1>
+                <h1>Contact Us</h1>
                 <p>Do cupidatat officia do voluptate nostrud nisi. Reprehenderit amet aute sint consectetur qui aliqua occaecat culpa duis Lorem. Reprehenderit quis sint nostrud nostrud. Dolor quis tempor eiusmod in minim laborum sint elit enim et commodo nulla cupidatat fugiat.</p>
             </div>
+            <form name="contact" method="POST" data-netlify="true" action="/contact/successs">
+                <label>Name: <input type="text" name="name" placeholder="Name" /></label>
+                <label>Email: <input type="email" name="email" placeholder="Email" /></label>
+                <label>Message: <textarea name="message">Message</textarea></label>
+                <button type="submit">Send</button>
+            </form>
     </Layout>
 )
 
---
+++ src/pages/contact/success.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../../components/layout';
+
+const About = (location) => (
+    <Layout location={location}>
+        <div>
+            <h1>Success</h1>
+            <p>Thank you</p>
+        </div>
+    </Layout>
+)
+
+export default About
